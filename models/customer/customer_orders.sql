dc5789150d96434cc0ac5b14edd805444bd834f0
shoun623 <tsfmlml623@gmail.com>
2024-01-28 10:59:43 +0900

    first commit

---
+++ models/customer/customer_orders.sql
@@ -0,0 +1,10 @@
+SELECT
+    customer_id,
+
+    min(order_date) as first_order_date,
+    max(order_date) as most_recent_order_date,
+    count(order_id) as number_of_orders
+
+from {{ ref('orders') }}
+
+group by 1
